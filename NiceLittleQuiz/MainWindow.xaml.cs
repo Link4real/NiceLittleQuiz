92c9801aeaa63227082ba5fbbda948255f141ccf
Link4real <simon@die-duerrs.de>
2021-10-02 17:13:27 +0200

    Add project files.

---
+++ NiceLittleQuiz/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NiceLittleQuiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (text2.IsChecked == true && text5.IsChecked == true && text8.IsChecked == true)
+            {
+                MessageBox.Show("You Win!");
+            }
+            else
+            {
+                MessageBox.Show("You Lose.");
+            }
+        }
+    }
+}
+
